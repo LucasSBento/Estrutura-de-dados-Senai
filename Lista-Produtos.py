06b2ecffee8bc0a7ea353d0671e186af01b44382
Rafael-Saldanha <rafael.saldanha@aluno.senai.br>
2025-06-03 11:09:24 -0300

    Lista-Produto

--- Lista-Produtos.py
+++ Lista-Produtos.py
@@ -9,3 +9,27 @@ while (adicionar == "s"):
 vetor.sort()
 print(vetor)
 
+#lista_de_produtos (cliente)
+
+import sys
+import random
+vaga_desejada = input("deseja se candidatar a qual vaga? 1-Pedreiro 2-Programador 3-Engenheiro 4-youtuber: ")
+if (vaga_desejada == "1"):
+  print("Pedreiro")
+elif (vaga_desejada == "2"):
+  print("Programador")
+elif (vaga_desejada == "3"):
+  print("Engenheiro")
+elif (vaga_desejada == "4"):
+  print("youtuber")
+else:
+  sys.exit("vaga não encontrada")
+vetor = ["Marcos","Pablo_clone","Pablo","Luiz"]
+entrada = input("deseja se candidatar a vaga? s-sim n-não: ")
+while (entrada == "s"):
+  adicionando = input("Digite o seu nome: ")
+  print("inscrição efetuada")
+  vetor.append(adicionando)
+  break
+random.shuffle(vetor)
+print("lista de participantes: " ,vetor)
